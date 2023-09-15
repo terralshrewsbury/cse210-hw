1351941416973e37fc5ed0503019e7740556f857
Turrtle0612 <123220471+Turrtle0612@users.noreply.github.com>
2023-09-14 21:11:16 -0600

    Finished the assinment for W01

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string sign = "";
+        Console.WriteLine("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+        
+        if (int.TryParse(gradeInput, out int grade))
+        {
+            int onesplace = grade % 10;
+            
+            if (onesplace < 3 )
+            {
+                sign = "-";
+            }
+            
+            else if (onesplace > 7 )
+            {
+                sign = "+";
+            } 
+            
+            if (grade >= 70)
+            {
+                Console.WriteLine("You passed the class!");
+                    
+                if (grade >= 90)
+                {
+                    if (sign == "" && sign == "-")
+                    {
+                    Console.WriteLine($"You have an {sign}A");
+                    }
+        
+                    else
+                    {
+                    Console.WriteLine("You have an A");
+                    }                                    
+                }          
+                
+                else if (grade >= 80)
+                {
+                    Console.WriteLine($"You have a {sign}B");
+                }
+                
+                else if (grade >= 70)
+                {
+                    Console.WriteLine($"You have a {sign}C");
+                }
+            }    
+                
+            else
+            {
+                Console.WriteLine("You must take the class again to learn what you need to pass.");
+                
+                if (grade >= 60)
+                {
+                    Console.WriteLine($"You have a {sign}D");
+                }
+                else
+                {
+                Console.WriteLine("You have an F");
+                }
+            }                    
+        }
     }
 }
+    
