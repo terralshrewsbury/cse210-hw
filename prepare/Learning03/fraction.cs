db650b6b0b64e1a7cbe541ed1df348d0427784ef
Turrtle0612 <123220471+Turrtle0612@users.noreply.github.com>
2023-10-19 13:33:52 -0600

    finished encapsulation learning activity w05

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+public class Fraction 
+{
+    private int numerator {get; set;}
+    private int denominator {get; set;}
+    
+    public int Numerator
+    {
+        get {return numerator;}
+        set {numerator = value;}
+    }
+
+    public int Denominator
+    {
+        get {return denominator;}
+        set {if (value != 0) 
+            {
+                denominator = value;
+            }
+            else
+            {
+                Console.WriteLine("denominator can not = 0.");
+            }
+        }
+    }
+
+    public Fraction ()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Fraction (int top)
+    {
+        numerator = top;
+        denominator = 1;
+    }
+
+    public Fraction (int top, int bottom)
+    {
+        numerator = top;
+        denominator = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return numerator + "/" + denominator;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator/denominator;
+    }
+
+}
